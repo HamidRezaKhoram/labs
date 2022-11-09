643b227c74a296b6e0f1723a8cba982b97edeea5
HamidReza <hr.kh1380@gmail.com>
2022-11-09 11:23:21 -0500

    modified:   lab7/lab7.py

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -3,25 +3,39 @@
 # lab7
 
 import random
-
+import math
 def random_die():
     inRun = False
-    list_dies = []
+    list_dies = [random.randrange(1,7) for _ in range(20)]
     
-    list_dies.append(random.randrange(1,7))
+    print(list_dies)
     
     for i in range(20):
-        list_dies.append(random.randrange(1,7))
-        if list_dies[i] == list_dies[i+1] and inRun == False:
-            print('(', end=' ')
-            inRun = True
+
+        if i <= 18:
+            if list_dies[i] == list_dies[i+1] and inRun == False:
+                print('(', end=' ')
+                inRun = True
             print(list_dies[i], end=' ')
+            if inRun:
+                if list_dies[i] != list_dies[i+1]:
+                    print(')', end=' ')
+                    inRun = False
         else:
-            print(list_dies[i], end=' ')
-        if list_dies[i] != list_dies[i+1] and inRun == True:
-            print(' )', end='')
-            inRun = False
+            if list_dies[18] == list_dies[19] and inRun == False:
+                print('(', end=' ')
+                inRun = True
             
+            print(list_dies[19], end=' ')
+            if inRun:
+                if list_dies[18] != list_dies[19]:
+                    print(')', end=' ')
+                    inRun = False
+        
+        
+    if inRun:
+        print(')', end='')
+             
     print('')
 def seqgen():
     list_dies = [str(random.randrange(1,7)) for i in range(20)]
@@ -48,7 +62,7 @@ def seqgen():
 
 def longestFalse(L):
     
-    holder = ()
+    holder = tuple()
     
     for i in range(len(L)):
         list_tuple = [()]
@@ -65,26 +79,49 @@ def longestFalse(L):
                 check = False
         if L[-1] == False:
             list_tuple[counter] += (len(L)-1, )
-    print(list_tuple)
+    # print(list_tuple)
     longestFalse = -1
-    holder2 = list_tuple[0]
     for i in list_tuple:
         if len(i) > 1:
             if abs(i[0]-i[1]) >= longestFalse and abs(i[0]-i[1]) != longestFalse:
                 longestFalse = abs(i[0]-i[1])
                 holder = i
+    return holder
     print(holder)
-def birdNest():
-    listNest = ['-' for _ in range(10)]
-    for i in range(len(listNest)):
-        print(listNest, end='')
-        
-                 
+import math
+def birdNest(n):
+    listNest = ['-' for _ in range(n)]
+    def get_false(dash):
+        temp_false = []
+        for i in dash:
+            if i == '-':
+                temp_false.append(False)
+            else:
+                temp_false.append(True)
+        return temp_false
+    # print(get_false(listNest))
 
+    print(' '.join(listNest))
+    for i in range(len(listNest)):
+        biggest = longestFalse(get_false(listNest))
+        # print(type(biggest))
+        pointA, pointB = biggest[0], biggest[1]
+        birdSpot = (pointA + pointB)/2
+        listNest[math.ceil(birdSpot)] = 'x'
+        print(' '.join(listNest))
+    
+
+def isPal(L):
+    listL = L
+    listL.reverse()
+    print(listL)
+    return listL == L[::-1]
+    print(l_s_r)
 if __name__ == '__main__':
-    # random_die()
+    random_die()
     # seqgen()
     # longestFalse([True, True, False, False, True, False, True, True, False, False, True, True])
-    birdNest()
+    # birdNest(4)
+    # print(isPal([5, 1, 9, 9, 1, 5]))
     
     
